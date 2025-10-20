d3266f199fd1bcf443cf55673fe4f38bfaec2037
mejivich <senzermen@gmail.com>
2025-10-20 15:44:40 +0000

    Update workspace with current ROS2 packages

--- src/ur_yt_sim/setup.py
+++ src/ur_yt_sim/setup.py
@@ -1,30 +1,51 @@
 from setuptools import setup
+import os
+from glob import glob
 
 package_name = 'ur_yt_sim'
 
 setup(
     name=package_name,
     version='0.0.0',
-    packages=[package_name],
+    packages=[],
+    py_modules=[],
     data_files=[
-        ('share/ament_index/resource_index/packages',
-            ['resource/' + package_name]),
-        ('share/' + package_name, [
-            'package.xml',
-            'launch/ur16e_with_moveit.launch.py',
-            'urdf/ur16e_camera.urdf.xacro',
-            'urdf/car_with_inlet.urdf',
-            'worlds/world2.world',
-            'config/ur16e_controllers.yaml',
-            'config/initial_positions.yaml',
-            'rviz/view_ur16e.rviz',
-        ]),
+        # Resource index
+        ("share/ament_index/resource_index/packages",
+         ["resource/" + package_name] if os.path.exists("resource/" + package_name) else []),
+
+        # Package.xml
+        ("share/" + package_name, ["package.xml"]),
+
+        # Launch files
+        ("share/" + package_name + "/launch", glob("launch/*.launch.py")),
+
+        # URDF/XACRO
+        ("share/" + package_name + "/urdf", glob("urdf/*.urdf") + glob("urdf/*.xacro")),
+
+        # Configs
+        ("share/" + package_name + "/config", glob("config/*.yaml")),
+
+        # RViz
+        ("share/" + package_name + "/rviz", glob("rviz/*.rviz")),
+
+        # Worlds
+        ("share/" + package_name + "/worlds", glob("worlds/*.world")),
+
+        # Meshes (recursive)
+        ("share/" + package_name + "/meshes", glob("meshes/**/*", recursive=True)),
+
+        # Gazebo models (recursive)
+        ("share/" + package_name + "/models", glob("models/**/*", recursive=True)),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
     maintainer='root',
     maintainer_email='root@todo.todo',
-    description='UR16e and car simulation package',
-    license='TODO',
+    description='UR16e + car + MoveIt + Gazebo integration',
+    license='MIT',
     tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [],
+    },
 )
