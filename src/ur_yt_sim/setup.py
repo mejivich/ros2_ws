2edd4d41ca72b6b3c3341685caaa7f7aaf459b9e
mejivich <senzermen@gmail.com>
2025-10-21 10:55:05 +0000

    Switch package from CMake ament to Python ament

--- src/ur_yt_sim/setup.py
+++ src/ur_yt_sim/setup.py
@@ -2,50 +2,40 @@ from setuptools import setup
 import os
 from glob import glob
 
+def recursive_files(path):
+    """Return a list of all files under path (ignore directories)."""
+    files = []
+    for root, _, filenames in os.walk(path):
+        for f in filenames:
+            files.append(os.path.join(root, f))
+    return files
+
 package_name = 'ur_yt_sim'
 
 setup(
     name=package_name,
     version='0.0.0',
-    packages=[],
-    py_modules=[],
+    packages=[package_name],
     data_files=[
-        # Resource index
-        ("share/ament_index/resource_index/packages",
-         ["resource/" + package_name] if os.path.exists("resource/" + package_name) else []),
-
-        # Package.xml
-        ("share/" + package_name, ["package.xml"]),
+        ('share/ament_index/resource_index/packages', ['package.xml']),
+        ('share/' + package_name, ['resource/' + package_name] if os.path.exists('resource/' + package_name) else []),
 
         # Launch files
-        ("share/" + package_name + "/launch", glob("launch/*.launch.py")),
+        ('share/' + package_name + '/launch', glob('launch/*.launch.py')),
 
-        # URDF/XACRO
-        ("share/" + package_name + "/urdf", glob("urdf/*.urdf") + glob("urdf/*.xacro")),
+        # URDF/XACRO files
+        ('share/' + package_name + '/urdf', glob('urdf/*.urdf') + glob('urdf/*.xacro')),
 
-        # Configs
-        ("share/" + package_name + "/config", glob("config/*.yaml")),
+        # Config files
+        ('share/' + package_name + '/config', glob('config/*.yaml')),
+        ('share/' + package_name + '/config/ur16e', glob('config/ur16e/*.yaml')),
 
-        # RViz
-        ("share/" + package_name + "/rviz", glob("rviz/*.rviz")),
-
-        # Worlds
-        ("share/" + package_name + "/worlds", glob("worlds/*.world")),
-
-        # Meshes (recursive)
-        ("share/" + package_name + "/meshes", glob("meshes/**/*", recursive=True)),
-
-        # Gazebo models (recursive)
-        ("share/" + package_name + "/models", glob("models/**/*", recursive=True)),
+        # Meshes, models, rviz, worlds — only files, recursive
+        ('share/' + package_name + '/meshes', recursive_files('meshes')),
+        ('share/' + package_name + '/models', recursive_files('models')),
+        ('share/' + package_name + '/rviz', glob('rviz/*.rviz')),
+        ('share/' + package_name + '/worlds', glob('worlds/*.world')),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
-    maintainer='root',
-    maintainer_email='root@todo.todo',
-    description='UR16e + car + MoveIt + Gazebo integration',
-    license='MIT',
-    tests_require=['pytest'],
-    entry_points={
-        'console_scripts': [],
-    },
 )
